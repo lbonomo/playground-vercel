dff76dc0a357c2a131dedd61f2e99fbb098b7e5d
Lucas Bonomo <bonomo.lucas@gmail.com>
2022-02-16 11:32:58 -0300

    Public?

---
+++ README.md
@@ -0,0 +1,11 @@
+# Playground AWS CloudFront + Vercel
+
+## Vercel
+Just an `Standalone Express` app
+
+
+```
+npm run dev
+```
+
+## CloudFront
--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 app.get('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     console.log(`Client IP: ${ip}`)
     res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
+
+module.exports = app;
