023645c5573dbf302e7a8b9309416e6f5821d4a7
Lucas Bonomo <bonomo.lucas@gmail.com>
2022-02-16 17:33:51 -0300

    Added favicon to prevent warnings on the console and stcript to load IP to remote service

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# Playground AWS CloudFront + Vercel
+# Juando con AWS CloudFront + Vercel
+El objetivo de este pequeño ejemplo es documentar el uso de CloudFront como chache de aplicaciones Verser
+## En Vercel
+En Vercel solo montamo un *Hola mundo* hecho con `Express`.
 
-## Vercel
-Just an `Standalone Express` app
-
-
-```
-npm run dev
-```
+Como dato importante, Vercel no esta pensado para ejecutar aplicaciones Node de forma nativa, por lo que es necesario agregar un archivo `vercel.json` en el directorio raiz del proyecto.
 
 ## CloudFront
+Uno de los objetivos principales del ejempo es obtener la IP del cliente que visita nuetra pagina del lado de Vercel y no la IP propia del CloudFront (que es lo que pasa por defecto), para esto es necesario configurar el parametro `CloudFront-Viewer-Address` en CloudFront.
--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 const express = require('express')
 const app = express()
+const path = require('path');
 
 app.get('/', (req, res) => {
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const xip = req.headers['x-client-ip']
-    res.send(`<p>Hello World!</br>Your IP is: ${ip} / ${xip}</p>`)
+    const body = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CloudFront + Vercel</title>  
+    <link rel="icon" type="image/ico" href="/public/images/favicon.ico"> 
+    <link rel="shortcut icon" type="image/ico" href="/public/images/favicon.ico"> 
+    <link rel="stylesheet" href="public/css/style.css">
+  </head>
+  <body>
+  <ul>
+  <li>Your public IP: <spam id="publicIP"></spam></li>
+  <li>Your public IP: ${ip}</li>
+  <li>Your public IP: ${xip}</li>
+  </ul>
+  <script>
+  fetch('https://vanguard.com.ar/wp-json/show-remote-ip/v1/get-ip')
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('publicIP').textContent = data
+    });
+  </script>
+  </body>
+</html>
+    `
+    res.setHeader(
+      'Content-Security-Policy',
+      "default-src 'self' 'unsafe-inline' https://vanguard.com.ar/wp-json/show-remote-ip/v1/get-ip;"
+    );
+    
+    res.send(body)
 })
+
+app.use('/public', express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
